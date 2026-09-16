2837a83a97be04c086e097f6a511227884831b88
Deepika-Desai <240631895+Deepika-Desai@users.noreply.github.com>
2026-09-17 00:22:45 +0530

    PrintingData

--- Basic codes.py
+++ Basic codes.py
@@ -1,2 +1,10 @@
 #We will start with basic codes in python.
 '''To start with the syntax... Python is kinda easy to understand and learn, we just have to remeber certain rules to write a code in python.'''
+
+#Printing something on the screen
+print("Hellow World")
+'''This is the syntax we use to print something on the output screen. (to run the code press ctrl+f5).
+   The is very easy, whenever we have to print anything we write "print()" and then add the values to be printed inside the bracket...
+   There're multiple types of data that we have in python but for now we'll just learn about a few data types...
+     String data type- it is a data type which we need to print as it is and hence they're enclosed in quotes. '''
+
