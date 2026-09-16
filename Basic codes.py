789993269f1e1e8dbd71dbaf4aa3830c034f671d
Deepika-Desai <240631895+Deepika-Desai@users.noreply.github.com>
2026-09-17 00:09:54 +0530

    Desc

--- Basic codes.py
+++ Basic codes.py
@@ -1,3 +1,2 @@
-#We will start with basic codes in python
-
-print("Hello world")
+#We will start with basic codes in python.
+'''To start with the syntax... Python is kinda easy to understand and learn, we just have to remeber certain rules to write a code in python.'''
